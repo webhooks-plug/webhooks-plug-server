d041fefd9bc022725d3bddb61010ae1d47789f23
davetech <davidrums65@gmail.com>
2024-01-15 23:27:55 +0100

    feat(services): add basic service scaffolding

--- functions/services/index.js
+++ functions/services/index.js
@@ -1,3 +1,37 @@
-export const handler = async () => {
+const { APIResponse, HTTP } = require("./config");
+
+const permittedRoutes = ["/services"];
+
+const handler = async (event) => {
+  const route = event.path;
+  const httpMethod = event.httpMethod;
+
+  if (!permittedRoutes.includes(route)) {
+    return {
+      statusCode: APIResponse.SERVER_ERROR,
+      body: JSON.stringify({
+        message: "Route is not permitted",
+      }),
+    };
+  }
+
+  switch (httpMethod) {
+    case HTTP.GET:
+      console.log("Get request");
+      break;
+  }
+
+  console.log(event);
   // create app wide services here
+  return {
+    statusCode: APIResponse.OK,
+    body: JSON.stringify({
+      status: APIResponse.OK,
+      message: "Successful response",
+    }),
+  };
+};
+
+module.exports = {
+  handler,
 };
