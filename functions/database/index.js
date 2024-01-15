8f6aa5494fe268cfb9e51184ff3560c459ae1c65
davetech <davidrums65@gmail.com>
2024-01-15 23:24:56 +0100

    feat(schema): import database file and issue queries using pg pool

--- functions/database/index.js
+++ functions/database/index.js
@@ -1,3 +1,21 @@
-export const handler = async () => {
+const fs = require("node:fs");
+const { pgPool } = require("./config");
+
+const sqlFilePath = "./database.sql";
+const sqlQuery = fs.readFileSync(sqlFilePath, "utf8");
+
+const handler = async () => {
   // import sql file and run query against database
+  try {
+    const poolClient = await pgPool.connect();
+    const query = await poolClient.query(sqlQuery);
+    console.log(query);
+    await pgPool.end();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  handler,
 };
