3fe367e4f45ec3c935f597f564d8734c2ef1718f
davetech <davidrums65@gmail.com>
2024-01-16 18:57:33 +0100

    feat(config): add pg pool connection info

--- functions/services/config.js
+++ functions/services/config.js
@@ -1,14 +1,34 @@
+const { Pool } = require("pg");
+
 const APIResponse = {
   OK: 200,
+  VALIDATION_FAILED: 401,
   SERVER_ERROR: 500,
 };
 
 const HTTP = {
   GET: "GET",
   POST: "POST",
+  DELETE: "DELETE",
+  PUT: "PUT",
 };
+
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+
+const pgPool = new Pool({
+  user,
+  host,
+  port,
+  password,
+  database,
+});
 
 module.exports = {
   HTTP,
+  pgPool,
   APIResponse,
 };
