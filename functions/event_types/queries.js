cfb5c7a8e2e0efcbc6d00af43e611858c5e3c05f
davetech <davidrums65@gmail.com>
2024-01-17 23:14:47 +0100

    feat(queries): add query to get service

--- functions/event_types/queries.js
+++ functions/event_types/queries.js
@@ -1,5 +1,6 @@
 const queries = {
   CREATE_EVENT_TYPE: `insert into event_types (name, service_id, topic_arn) values ($1, $2, $3) returning id, name, service_id, topic_arn, created_on`,
+  GET_SERVICE: `select id, name from services where deleted_on = ${zeroTimestamp} and id = $1`,
 };
 
 module.exports = {
