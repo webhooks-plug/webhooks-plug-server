47a07ceb8131c113e6d6d9a7fc4d07e4362ba4bb
davetech <davidrums65@gmail.com>
2024-01-18 22:14:40 +0100

    feat(event_types): add methods to get service and create event type

--- functions/event_types/utils.js
+++ functions/event_types/utils.js
@@ -1,16 +1,27 @@
 const { pgPool } = require("./config");
 const { queries } = require("./queries");
 
-const createEventType = async (serviceId, eventName) => {
+const getService = async (serviceId) => {
+  const poolClient = await pgPool.connect();
+  const service = await poolClient.query(queries.CREATE_EVENT_TYPE, [
+    serviceId,
+  ]);
+  await pgPool.end();
+  return service;
+};
+
+const createEventType = async (serviceId, eventName, topicArn) => {
   const poolClient = await pgPool.connect();
   const eventType = await poolClient.query(queries.CREATE_EVENT_TYPE, [
     eventName,
     serviceId,
+    topicArn,
   ]);
   await pgPool.end();
   return eventType;
 };
 
 module.exports = {
+  getService,
   createEventType,
 };
