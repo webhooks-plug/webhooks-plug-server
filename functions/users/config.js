789b1362a539413496185364f5c5daacdab378f6
davetech <davidrums65@gmail.com>
2024-01-19 23:10:37 +0100

    chore(config): update response codes

--- functions/users/config.js
+++ functions/users/config.js
@@ -1,8 +1,7 @@
-const { Pool } = require("pg");
-
 const APIResponse = {
   OK: 200,
-  VALIDATION_FAILED: 401,
+  VALIDATION_FAILED: 400,
+  UNAUTHORIZED: 401,
   SERVER_ERROR: 500,
 };
 
@@ -13,22 +12,7 @@ const HTTP = {
   PUT: "PUT",
 };
 
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const database = process.env.DB_NAME;
-const host = process.env.DB_HOST;
-const port = process.env.DB_PORT;
-
-const pgPool = new Pool({
-  user,
-  host,
-  port,
-  password,
-  database,
-});
-
 module.exports = {
   HTTP,
-  pgPool,
   APIResponse,
 };
