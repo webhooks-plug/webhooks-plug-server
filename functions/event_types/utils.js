c251c30dee97bd2ea4e0a9d939e80f8cf69d4756
davetech <davidrums65@gmail.com>
2024-01-19 23:05:24 +0100

    feat(event_types): add more CRUD operation methods

--- functions/event_types/utils.js
+++ functions/event_types/utils.js
@@ -1,27 +1,87 @@
-const { pgPool } = require("./config");
+const { createClient } = require("/opt");
 const { queries } = require("./queries");
 
+const isValidUUID = (uuid) => {
+  const uuidRegex =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+  return uuidRegex.test(uuid);
+};
+
 const getService = async (serviceId) => {
-  const poolClient = await pgPool.connect();
-  const service = await poolClient.query(queries.CREATE_EVENT_TYPE, [
-    serviceId,
-  ]);
-  await pgPool.end();
+  const poolClient = await createClient();
+  const service = await poolClient.query(queries.GET_SERVICE, [serviceId]);
   return service;
 };
 
 const createEventType = async (serviceId, eventName, topicArn) => {
-  const poolClient = await pgPool.connect();
+  const poolClient = await createClient();
   const eventType = await poolClient.query(queries.CREATE_EVENT_TYPE, [
     eventName,
     serviceId,
     topicArn,
   ]);
-  await pgPool.end();
+  return eventType;
+};
+
+const updateEventType = async (eventTypeId, eventName) => {
+  const poolClient = await createClient();
+  const eventType = await poolClient.query(queries.UPDATE_EVENT_TYPE, [
+    eventName,
+    eventTypeId,
+  ]);
+  return eventType;
+};
+
+const deleteEventType = async (eventTypeId) => {
+  const deleteSubsciptions = async () => {
+    const poolClient = await createClient();
+    const subscriptions = await poolClient.query(queries.DELETE_SUBSCRIPTION, [
+      eventTypeId,
+    ]);
+    return subscriptions;
+  };
+
+  await deleteSubsciptions();
+
+  const poolClient = await createClient();
+  const eventType = await poolClient.query(queries.DELETE_EVENT_TYPE, [
+    eventTypeId,
+  ]);
+  return eventType;
+};
+
+const getEventType = async (eventTypeId) => {
+  const poolClient = await createClient();
+  const eventType = await poolClient.query(queries.GET_EVENT_TYPE, [
+    eventTypeId,
+  ]);
+  return eventType;
+};
+
+const getEventTypeByName = async (name, serviceId) => {
+  const poolClient = await createClient();
+  const eventType = await poolClient.query(queries.GET_EVENT_TYPE_NAME, [
+    name,
+    serviceId,
+  ]);
+  return eventType;
+};
+
+const getEventTypes = async (serviceId) => {
+  const poolClient = await createClient();
+  const eventType = await poolClient.query(queries.GET_EVENT_TYPES, [
+    serviceId,
+  ]);
   return eventType;
 };
 
 module.exports = {
   getService,
   createEventType,
+  updateEventType,
+  deleteEventType,
+  getEventType,
+  getEventTypeByName,
+  isValidUUID,
+  getEventTypes,
 };
