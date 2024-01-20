01d9d7d7667d96ddd3f0b4cad52c5f5087d6c51f
davetech <davidrums65@gmail.com>
2024-01-20 22:41:10 +0100

    feat(events): add method to publish event

---
+++ functions/events/index.js
@@ -0,0 +1,58 @@
+const { AppResponse } = require("./api");
+const { APIResponse, HTTP } = require("./config");
+const { publishEvent, getEventType, isValidUUID } = require("./utils");
+
+const handler = async (event) => {
+  const httpMethod = event.httpMethod;
+  const body = JSON.parse(event.body);
+  const params = event.pathParameters;
+  const eventTypeId = params?.event_id;
+
+  try {
+    switch (httpMethod) {
+      case HTTP.POST:
+        const { event_type_name, message, user_id } = body;
+
+        if (!event_type_name || !message || !user_id) {
+          return AppResponse({
+            message: "Event type name, message and user ID is required",
+            status: APIResponse.VALIDATION_FAILED,
+          });
+        }
+
+        if (!isValidUUID(user_id)) {
+          return AppResponse({
+            message: "User ID is not a valid uuid",
+            status: APIResponse.VALIDATION_FAILED,
+          });
+        }
+
+        const eventType = await getEventType(event_type_name);
+
+        if (eventType.rowCount === 0) {
+          return AppResponse({
+            message: "Event type name is invalid",
+            status: APIResponse.VALIDATION_FAILED,
+          });
+        }
+
+        const event = await publishEvent(eventType.rows[0], message, user_id);
+
+        return AppResponse({
+          message: "Event published successfully",
+          data: event,
+        });
+    }
+  } catch (err) {
+    console.log(err);
+    return AppResponse({
+      message: "Server error occured",
+      status: APIResponse.SERVER_ERROR,
+      error: err,
+    });
+  }
+};
+
+module.exports = {
+  handler,
+};
