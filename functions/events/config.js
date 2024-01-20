13fd99dbe8e427892d12a4600535b84b5d2864b1
davetech <davidrums65@gmail.com>
2024-01-20 22:40:39 +0100

    chore(config): add http methods object

---
+++ functions/events/config.js
@@ -0,0 +1,17 @@
+const APIResponse = {
+  OK: 200,
+  VALIDATION_FAILED: 401,
+  SERVER_ERROR: 500,
+};
+
+const HTTP = {
+  GET: "GET",
+  POST: "POST",
+  DELETE: "DELETE",
+  PUT: "PUT",
+};
+
+module.exports = {
+  HTTP,
+  APIResponse,
+};
