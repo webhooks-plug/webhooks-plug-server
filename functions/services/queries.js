99e246bd8117c49ff8b5bcc640e69b2cb5a3601f
davetech <davidrums65@gmail.com>
2024-01-21 22:28:25 +0100

    feat(services): update get service query

--- functions/services/queries.js
+++ functions/services/queries.js
@@ -3,7 +3,7 @@ const zeroTimestamp = "1970-01-01T00:00:00.000Z";
 const queries = {
   CREATE_SERVICE: `insert into services (name) values ($1) returning id, name, created_on`,
   LIST_SERVICES: `select id, name, created_on from services where deleted_on = '${zeroTimestamp}'`,
-  GET_SERVICE: `select id, name from services where deleted_on = '${zeroTimestamp}' and id = $1`,
+  GET_SERVICE: `select id, name, created_on from services where deleted_on = '${zeroTimestamp}' and id = $1`,
   GET_SERVICE_NAME: `select id, name from services where deleted_on = '${zeroTimestamp}' and name = $1`,
   DELETE_USERS: `update users set deleted_on = now() where users.service_id = $1 and users.deleted_on = '${zeroTimestamp}'`,
   DELETE_ENDPOINTS: `update endpoints set deleted_on = now() from users where users.id = endpoints.user_id and users.service_id = $1 and users.deleted_on = '${zeroTimestamp}'`,
