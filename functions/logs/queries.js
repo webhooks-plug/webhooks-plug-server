661b73429f0435decef4e7bc07bec861372ef20b
davetech <davidrums65@gmail.com>
2024-01-21 22:26:52 +0100

    feat(logs): add update message query

--- functions/logs/queries.js
+++ functions/logs/queries.js
@@ -1,7 +1,7 @@
 const zeroTimestamp = "1970-01-01T00:00:00.000Z";
 
 const queries = {
-  UPDATE_ENDPOINT: `update messages set delivered_at = $1, delivery_attempts = delivery_attempts + 1, status = $2 where endpoint = $3 and event_id = $4 and deleted_on = '${zeroTimestamp}'`,
+  UPDATE_MESSAGE: `update messages set delivered_at = $1, delivery_attempts = delivery_attempts + 1, status = $2 where endpoint = $3 and sns_message_id = $4 and deleted_on = '${zeroTimestamp}'`,
 };
 
 module.exports = {
