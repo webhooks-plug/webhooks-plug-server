94a61ebe8adb2fe08c630cda9b1410954fd509b9
davetech <davidrums65@gmail.com>
2024-01-22 22:32:26 +0100

    feat(users): add headers to app response

--- functions/users/api.js
+++ functions/users/api.js
@@ -7,6 +7,12 @@ const AppResponse = ({
   status = APIResponse.OK,
 }) => {
   return {
+    statusCode: status,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
+    },
     body: JSON.stringify({
       error,
       data,
