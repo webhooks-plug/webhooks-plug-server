3d3ff7c737159fdf227da957296a46a892b82ed5
davetech <davidrums65@gmail.com>
2024-01-22 22:29:21 +0100

    feat(event_types): add get event types query

--- functions/event_types/queries.js
+++ functions/event_types/queries.js
@@ -7,7 +7,7 @@ const queries = {
   DELETE_EVENT_TYPE: `update event_types set deleted_on = now() where id = $1 returning id, name, service_id, topic_arn, created_on`,
   GET_EVENT_TYPE: `select id, name, service_id, topic_arn from event_types where id = $1 and deleted_on = '${zeroTimestamp}'`,
   GET_EVENT_TYPE_NAME: `select id, name, service_id, topic_arn from event_types where name = $1 and service_id = $2 and deleted_on = '${zeroTimestamp}'`,
-  GET_EVENT_TYPES: `select e.id, e.name, s.name, e.topic_arn from event_types e join services s on s.id = e.service_id where s.id = $1 and s.deleted_on = '${zeroTimestamp}' and e.deleted_on = '${zeroTimestamp}' `,
+  GET_EVENT_TYPES: `select e.id, e.name, s.name "serviceName", e.topic_arn from event_types e join services s on s.id = e.service_id where s.id = $1 and s.deleted_on = '${zeroTimestamp}' and e.deleted_on = '${zeroTimestamp}' `,
   GET_SERVICE: `select id, name from services where deleted_on = '${zeroTimestamp}' and id = $1`,
 };
 
