387f79463ad8e09c9586914271bb42975c2e315b
davetech <davidrums65@gmail.com>
2024-01-23 22:50:00 +0100

    feat(messages): add app configs

---
+++ functions/messages/config.js
@@ -0,0 +1,18 @@
+const APIResponse = {
+  OK: 200,
+  VALIDATION_FAILED: 400,
+  UNAUTHORIZED: 401,
+  SERVER_ERROR: 500,
+};
+
+const HTTP = {
+  GET: "GET",
+  POST: "POST",
+  DELETE: "DELETE",
+  PUT: "PUT",
+};
+
+module.exports = {
+  HTTP,
+  APIResponse,
+};
