227e3f6f4e0f4765ad56609caabea2ba3eff98b3
davetech <davidrums65@gmail.com>
2024-01-23 23:02:08 +0100

    feat(subscriptions): add get subscriptions query

--- functions/subscriptions/queries.js
+++ functions/subscriptions/queries.js
@@ -5,7 +5,7 @@ const queries = {
   UPDATE_ENDPOINT: `update endpoints set url = $1 where endpoints.id = $2 and endpoints.deleted_on = '${zeroTimestamp}'`,
   CREATE_SUBSCRIPTION: `insert into subscriptions (event_type_id, endpoint_id, subscription_arn, user_id) values ($1, $2, $3, $4) returning id, event_type_id, endpoint_id, created_on`,
   GET_SUBSCRIPTION: `select sub.id, et.name as "eventTypeName", en.url as "endpointUrl" from subscriptions sub join endpoints en on en.id = sub.endpoint_id join event_types et on et.id = sub.event_type_id where sub.deleted_on = '${zeroTimestamp}' and sub.id = $1`,
-  GET_SUBSCRIPTIONS: `select sub.id, et.name as "eventTypeName", en.url as "endpointUrl" from subscriptions sub join endpoints en on en.id = sub.endpoint_id join event_types et on et.id = sub.event_type_id where sub.deleted_on = '${zeroTimestamp}' and et.id = $1`,
+  GET_SUBSCRIPTIONS: `select sub.id, et.name as "eventTypeName", en.url as "endpointUrl" from subscriptions sub join endpoints en on en.id = sub.endpoint_id join event_types et on et.id = sub.event_type_id where sub.deleted_on = '${zeroTimestamp}' and et.id = $1 order by sub.created_on desc`,
   GET_SUBSCRIPTIONS_USER: `select sub.id, u.name as "nameOfUser", et.name as "eventTypeName", en.url as "endpointUrl" from subscriptions sub join endpoints en on en.id = sub.endpoint_id join event_types et on et.id = sub.event_type_id join users u on u.id = sub.user_id where sub.deleted_on = '${zeroTimestamp}' and sub.user_id = $1`,
   GET_EVENT_TYPE: `select id, name, topic_arn from event_types where id = $1 and deleted_on = '${zeroTimestamp}'`,
   GET_USER: `select id, name, service_id, created_on from users where deleted_on = '${zeroTimestamp}' and id = $1`,
