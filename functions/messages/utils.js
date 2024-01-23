961c8c002a671cef4c2c04e23ed15860dbf240fb
davetech <davidrums65@gmail.com>
2024-01-23 22:51:50 +0100

    feat(messages): add list messages method

--- functions/messages/utils.js
+++ functions/messages/utils.js
@@ -1,7 +1,7 @@
 const { createClient } = require("/opt");
 const { queries } = require("./queries");
 
-const listMessages = async (name) => {
+const listMessages = async () => {
   const poolClient = await createClient();
   const messages = await poolClient.query(queries.LIST_MESSAGES);
   poolClient.release();
