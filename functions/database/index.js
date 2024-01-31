e1581f7f7c73c43762811f7d86501ceef4b9ef88
davetech <davidrums65@gmail.com>
2024-02-01 00:25:56 +0100

    chore(cleanup): clean out comments

--- functions/database/index.js
+++ functions/database/index.js
@@ -5,10 +5,9 @@ const sqlFilePath = "./database.sql";
 const sqlQuery = fs.readFileSync(sqlFilePath, "utf8");
 
 const handler = async () => {
-  // import sql file and run query against database
   try {
     const poolClient = await pgPool.connect();
-    const query = await poolClient.query(sqlQuery);
+    await poolClient.query(sqlQuery);
     await pgPool.end();
   } catch (err) {
     console.log(err);
